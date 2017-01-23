2b7ff447ef24bf09da3ed277e84bbe5be3d1bf0e
matthew jenkins <drakaven@hotmail.com>
2017-01-23 22:06:12 +0000

    styled new tweet box

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,26 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
 
+  $('#form1').on('submit', function (event) {
+    event.preventDefault();
+    let newData = $(this).serialize();
+    $.post("/tweets",
+      newData,
+      //success callback
+      function (data, status) {
+        $.get("/tweets", function (data, status) {
+
+          let newText = "";
+          console.log(data);
+          data.forEach((item) => {
+            newText += item.content.text;
+          });
+          $("#p2").text(newText);
+        });
+      });
+  });
+
+//document ready close tag
+});
