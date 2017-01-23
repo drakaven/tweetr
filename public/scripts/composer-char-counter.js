a2a3b62eff84c29cfdbb9f814761b4f2d87f89ce
matthew jenkins <drakaven@hotmail.com>
2017-01-23 22:38:05 +0000

    added counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').on('input', function(){
+    let remaining = 140 - this.value.length;
+    let counter = $(this).siblings('.counter');
+   counter.text(remaining);
+    (remaining < 0) ? counter.addClass('red') : counter.removeClass('red');
+  });
+});
