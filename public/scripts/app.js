f375753396fc406db903b85a3aedd3336a650862
matthew jenkins <drakaven@hotmail.com>
2017-01-24 01:29:46 +0000

    added ajax temp

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function (tweetObj) {
+
+  const createTweetElement = function (tweetObject) {
+    let newArticle = $('<article id="tweet1" class="tweet"></article>');
+    newArticle[0].innerHTML = `<header>
+      <img id="tweeterAvatar" href=${tweetObject.user.avatars.small}>
+      <span id="tweeterName">${tweetObject.user.name}</span>
+      <span id="tweeterId">${tweetObject.user.handle}</span>
+      </header>
+      <p class="tweetText">${tweetObject.content.text}</p>
+      <footer>
+      <span class="postedDate">${tweetObject.created_at}</span>
+      </footer>`;
+    return newArticle[0];
+  };
+
+  const renderTweets = function (tweetData) {
+    let tweetContainer = $('#tweetContainer');
+    for (tweet in tweetData) {
+      tweetContainer.append(createTweetElement(tweetData[tweet]));
+    }
+  };
+  //self executing
+  const loadTweets = function () {
+    $.get("/tweets", function (data, status) {
+      renderTweets(data);
+    });
+  }();
+  console.log(loadTweets);
 
   $('#form1').on('submit', function (event) {
     event.preventDefault();
     let newData = $(this).serialize();
     $.post("/tweets",
       newData,
-      //success callback
-      function (data, status) {
+      function () {
         $.get("/tweets", function (data, status) {
-
-          let newText = "";
-          console.log(data);
-          data.forEach((item) => {
-            newText += item.content.text;
-          });
-          $("#p2").text(newText);
+          renderTweets(data);
         });
       });
   });
-
 //document ready close tag
 });
